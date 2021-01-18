97385cc2e4adb5395d8c4dc7f3ce0ed89c094cd2
josejunquera <jose.junquera@hotmail.com>
2021-01-18 18:26:38 +0100

    New controllers added to cars-case

--- cars-case/controllers/cars/create-car.js
+++ cars-case/controllers/cars/create-car.js
@@ -1,12 +1,25 @@
 const { create } = require("../../repositories/cars-repository");
+const Joi = require("joi");
+
+const schema = Joi.object().keys({
+  brand: Joi.string().required().min(2),
+  model: Joi.string().required(),
+  year: Joi.number().positive(),
+});
 
 function createCar(req, res) {
-  const { brand, model, year } = req.body;
+  try {
+    Joi.assert(req.body, schema);
+    const { brand, model, year } = req.body;
 
-  const car = { brand, model, year };
-  const addedCar = create(car);
+    const car = { brand, model, year };
+    const addedCar = create(car);
 
-  res.status(201).send(addedCar);
+    res.status(201).send(addedCar);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).send({ error: err.message });
+  }
 }
 
 module.exports = createCar;
